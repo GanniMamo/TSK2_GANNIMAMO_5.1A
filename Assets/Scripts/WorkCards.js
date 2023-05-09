bfc09fdcde5b2bca61ffe3730af88e7710bd32ec
GanniMamo <91873995+GanniMamo@users.noreply.github.com>
2023-05-09 15:05:28 +0200

    FIXED issue mainly of work page
    fixed js.

--- Assets/Scripts/WorkCards.js
+++ Assets/Scripts/WorkCards.js
@@ -3,21 +3,38 @@ var cardWidth = $('.carousel-item').width();
 
 var scrollPosition = 0;
 
-$('.carousel-control-next').on('click', function(){
+
+$('.next1').on('click', function(){
     if(scrollPosition < (carouselwidth - (cardWidth * 4))) {
         console.log('next');
         scrollPosition = scrollPosition + cardWidth;
-        $('.carousel-inner').animate({scrollLeft: scrollPosition}, 
+        $('.inner1').animate({scrollLeft: scrollPosition}, 
         600);
 }
 });
-$('.carousel-control-prev').on('click', function(){
+$('.prev1').on('click', function(){
     if(scrollPosition > 0){
         console.log('prev');
         scrollPosition = scrollPosition - cardWidth;
-        $('.carousel-inner').animate({scrollLeft: scrollPosition}, 
+        $('.inner1').animate({scrollLeft: scrollPosition}, 
         600);
 }
 });
 
+$('.next2').on('click', function(){
+    if(scrollPosition < (carouselwidth - (cardWidth * 4))) {
+        console.log('next');
+        scrollPosition = scrollPosition + cardWidth;
+        $('.inner2').animate({scrollLeft: scrollPosition}, 
+        600);
+}
+});
+$('.prev2').on('click', function(){
+    if(scrollPosition > 0){
+        console.log('prev');
+        scrollPosition = scrollPosition - cardWidth;
+        $('.inner2').animate({scrollLeft: scrollPosition}, 
+        600);
+}
+});
 
